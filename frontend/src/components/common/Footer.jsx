b23056335652074541ac709b2fdd0901c9389057
doctorzeee <dimasrafif11@gmail.com>
2025-10-13 00:30:48 +0700

    Initial commit: Laravel + React project

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="container py-5">
+                <div className="row">
+                    <div className="col-md-3">
+                        <h3>Dimas Web</h3>
+                        <div className="pe-5"></div>
+                        <p>
+                            Our post-construction services ives you peace of
+                            mind knowing that we are still here for you even
+                            after
+                        </p>
+                    </div>
+
+                    <div className="col-md-3">
+                        <h3 className="mb-3">Our Services</h3>
+                        <ul>
+                            <li>
+                                <a href="">Specialty Construction</a>
+                            </li>
+                            <li>
+                                <a href="">Specialty Construction</a>
+                            </li>
+                            <li>
+                                <a href="">Specialty Construction</a>
+                            </li>
+                            <li>
+                                <a href="">Specialty Construction</a>
+                            </li>
+                            <li>
+                                <a href="">Specialty Construction</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="col-md-3">
+                        <h3 className="mb-3">Quick Links</h3>
+                        <ul>
+                            <li>
+                                <a href="">About Us</a>
+                            </li>
+                            <li>
+                                <a href="">Services</a>
+                            </li>
+                            <li>
+                                <a href="">Projects</a>
+                            </li>
+                            <li>
+                                <a href="">Blogs</a>
+                            </li>
+                            <li>
+                                <a href="">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="col-md-3">
+                        <h3 className="mb-3">Contact Us</h3>
+                        <ul>
+                            <li>
+                                <a href="">(0812-3456-7890)</a>
+                            </li>
+                            <li>
+                                <a href="">google@gmail.com</a>
+                            </li>
+                            <p>
+                                B35 Perum Mentari Jaya <br /> Sumbang, Banyumas,
+                                56789
+                            </p>
+                        </ul>
+                    </div>
+                    <hr />
+                    <div className="text-center pt-4">
+                        Copyright © 2025 Dimas Web. All Rights Reserved.
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ frontend/src/components/frontend/Home.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import AboutImg from "../../assets/images/about-us.jpg";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+
+export const Home = () => {
+    return (
+        <>
+            <Header/>
+            <main>
+                {/* Hero Section */}
+                <section className="section-1">
+                    <div className="hero d-flex align-items-center">
+                        <div className="container-fluid">
+                            <div className="text-center">
+                                <span>Welcome Amazing Construction</span>
+                                <h1>
+                                    Crafting dreams with <br /> precision and
+                                    excelence
+                                </h1>
+                                <p>
+                                    We excel at transforming vision into reality
+                                    through outstanding craftmanship and precise{" "}
+                                    <br /> attention to detail. With years of
+                                    experience and a dedication to quality.
+                                </p>
+                                <div className="mt-4">
+                                    <a className="btn btn-primary">
+                                        Contact Now
+                                    </a>
+                                    <a className="btn btn-secondary ms-2">
+                                        View Projects
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* About Us Section */}
+                <section className="section-2 py-5">
+                    <div className="container py-5">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <img src={AboutImg} className="w-100" />
+                            </div>
+
+                            <div className="col-md-6">
+                                <span>About Us</span>
+                                <h2>
+                                    Crafting structures that last a lifetime
+                                </h2>
+                                <p>
+                                    Building enduring structures requires
+                                    comprehensive aproach that combines advanced
+                                    materials, resilient design, routine
+                                    maintence, and sustainable practices. By
+                                    drawing on historical insight and utilizing
+                                    modern technology.
+                                </p>
+                                <p>
+                                    Designing structures that stand he test of
+                                    the time involves a seamless blend of
+                                    cutting-edge materials, durable design,
+                                    ongoing upkeep, and eco-friendly practices.
+                                    By combining lesson from the past with the
+                                    power of modern technology
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <Footer/>
+        </>
+    );
+};
+
+export default Home;
